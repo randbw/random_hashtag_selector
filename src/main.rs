db8998d39ac28313b38377426be3e3fb661092cd
Andrew Ward <apbward@gmail.com>
2020-11-14 20:21:38 +0100

    accumulate randomised hashtags in set and print to console

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
+use rand::Rng;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::vec::Vec;
+use std::iter::FromIterator;
 
 fn main() {
-    let mut hashtags = HashSet::new();
+    let mut read_hashtags = HashSet::new();
+    let mut rng = rand::thread_rng();
+    let mut hashtags_to_use = HashSet::new();
 
     if let Ok(lines) = read_lines("./hashtags.txt") {
         for line in lines {
             if let Ok(hashtag) = line {
-                hashtags.insert(hashtag.to_string());
+                read_hashtags.insert(hashtag.to_string());
             }
         }
 
-        for hashtag in hashtags {
-            println!("hashytag is {}", hashtag);
+        let hashtags_veccaz = Vec::from_iter(read_hashtags.iter().cloned());
+
+        while hashtags_to_use.len() < 30 {
+            let index = rng.gen_range(0, read_hashtags.len());
+            hashtags_to_use.insert(hashtags_veccaz[index].to_string());
+            println!("hash at ind {} is {}", index, hashtags_veccaz[index].to_string());
         }
 
     } else {
