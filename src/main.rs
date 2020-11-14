d6374708967954c02eca5d430a2fcdaa32c7bd6f
Andrew Ward <apbward@gmail.com>
2020-11-14 20:37:43 +0100

    Concatenate selected hashtags in list prefixed with '#'

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     let mut read_hashtags = HashSet::new();
     let mut rng = rand::thread_rng();
     let mut hashtags_to_use = HashSet::new();
+    let mut hashtags_to_print = String::new();
 
     if let Ok(lines) = read_lines("./hashtags.txt") {
         for line in lines {
@@ -25,6 +26,14 @@ fn main() {
             hashtags_to_use.insert(hashtags_veccaz[index].to_string());
             println!("hash at ind {} is {}", index, hashtags_veccaz[index].to_string());
         }
+
+        for hashtag in hashtags_to_use {
+            hashtags_to_print.push_str(" #");
+            hashtags_to_print.push_str(&hashtag);
+        }
+
+        println!("Final hashtag list is:");
+        println!("{}", hashtags_to_print.trim_start());
 
     } else {
         println!("Failed to read ./hashtags.txt");
