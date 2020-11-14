a227c1ab7763cdc2f28b6dd2b580e04beab804bb
Andrew Ward <apbward@gmail.com>
2020-11-14 19:23:15 +0100

    Add hashtags to HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
+    let mut hashtags = HashSet::new();
+
     if let Ok(lines) = read_lines("./hashtags.txt") {
         for line in lines {
             if let Ok(hashtag) = line {
-                println!("{}", hashtag);
+                hashtags.insert(hashtag.to_string());
             }
         }
-    }
-    else {
-        println!("CHICKEN");
+
+        for hashtag in hashtags {
+            println!("hashytag is {}", hashtag);
+        }
+
+    } else {
+        println!("Failed to read ./hashtags.txt");
     }
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
